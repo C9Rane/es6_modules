a23b7ce0b2ba1d4339d1eb0f05ab7d35ad9568e4
Justin Campbell <Justin.Campbell92@hotmail.com>
2023-07-12 21:06:16 -0500

    bug slayer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-console.log("Hello World");
-
-// TODO
 import WishList from "./wishlist.js";
 
 const form = document.querySelector('#submitForm');
@@ -11,105 +8,38 @@ const makeParagraph = document.getElementById('car-make');
 const modelParagraph = document.getElementById('car-model');
 const yearParagraph = document.getElementById('car-year');
 const removeButton = document.querySelector('.removeBtn');
-const wishlistElement = document.querySelector('#wishListContainer ul');
+const wishlistElement = document.querySelector('#wishListContainer > ul');
 const wishlist = new WishList();
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let make = event.target[0].value;
-  let model = event.target[1].value;
-  let year = event.target[2].value;
-  wishlist.add(make, model, year);
-});
-
-wishlistElement.addEventListener('click', (event) => {
-  if (event.target.classList.contains('removeBtn')) {
-    const carId = event.target.dataset.carId;
-    wishlist.remove(carId);
-    renderWishlist();
-  }
-});
-
-function renderWishlist() {
-  wishlistElement.innerHTML = '';
-  wishlist.list.forEach(car => {
-    const li = document.createElement('li');
-    li.textContent = `${car.make} ${car.model} (${car.year})`;
-    const removeBtn = document.createElement('button');
-    removeBtn.classList.add('btn', 'btn-danger', 'removeBtn');
-    removeBtn.textContent = 'Remove';
-    removeBtn.dataset.carId = car.id;
-    li.appendChild(removeBtn);
-    wishlistElement.appendChild(li);
-  });
-
-  if (wishlist.list.length > 0) {
-    removeButton.disabled = false;
-  } else {
-    removeButton.disabled = true;
-  }
-}
-
 function showCarDetails(car) {
-    makeParagraph.textContent = "";
-    modelParagraph.textContent = "";
-    yearParagraph.textContent = "";
+    makeParagraph.textContent = car.make;
+    modelParagraph.textContent = car.model;
+    yearParagraph.textContent = car.year;
     removeButton.disabled = false;
-    removeButton.setAttribute('data-carId', car.id);
+    removeButton.setAttribute("data-carId", car.id);
 }
-  
-wishlistElement.addEventListener('click', e => {
-    if (e.target.classList.contains('removeBtn')) {
-      const carId = e.target.dataset.carId;
-      wishlist.remove(carId);
-      renderWishlist();
-      makeParagraph.textContent = '';
-      modelParagraph.textContent = '';
-      yearParagraph.textContent = '';
-      removeButton.disabled = true;
-      removeButton.removeAttribute('data-carId');
-    } else {
-      const carId = e.target.dataset.carId;
-      const car = wishlist.list.find(car => car.id === carId);
-      showCarDetails(car);
-    }
-  });
 
-  
 function updateDOMList() {
-    wishlistElement.innerHTML = '';
-      wishlist.list.forEach(car => {
-      const li = document.createElement('li');
-      li.textContent = `${car.make} ${car.model}`;
-      li.addEventListener('click', () => showCarDetails(car));
-      wishlistElement.appendChild(li);
+    wishlistElement.textContent = '';
+    wishlist.list.forEach((car) => {
+        const li = document.createElement('li');
+        li.textContent = `${car.make} ${car.model}`;
+        li.addEventListener("click", () => showCarDetails(car));
+        wishlistElement.appendChild(li);
     });
-  }
-  
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-    const make = makeInput.value;
-    const model = modelInput.value;
-    const year = yearInput.value;
-    wishlist.add(make, model, year);
-    renderWishlist();
-    updateDOMList();
-    form.reset();
-  });
+}
 
-  function addCar(event) {
+function addCar(event) {
     event.preventDefault();
     const make = makeInput.value;
     const model = modelInput.value;
     const year = yearInput.value;
+    console.log(make, model, year);
     wishlist.add(make, model, year);
     updateDOMList();
-    form.reset();
-  }
+}
 
-  form.addEventListener('submit', addCar);
-
-  function removeCar() {
+function removeCar() {
     const carId = Number(removeButton.getAttribute('data-carId'));
     wishlist.remove(carId);
     updateDOMList();
@@ -117,6 +47,16 @@ function updateDOMList() {
     modelParagraph.textContent = '';
     yearParagraph.textContent = '';
     removeButton.disabled = true;
-  }
-  
-  removeButton.addEventListener('click', removeCar);
+}
+
+/////
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addCar(event);
+});
+
+
+removeButton.addEventListener('click', () => {
+  removeCar();
+});
--- src/wishList.js
+++ src/wishList.js
@@ -1,44 +1,17 @@
-import Car from "./car";
+import Car from "./car.js";
 
 export default class WishList {
-    constructor(list = [], nextId = 0) {
-        this.list = list;
-        this.nextId = nextId;
+    constructor() {
+        this.list = [];
+        this.nextId = 0;
     }
+
     add(make, model, year) {
-    let car = new Car(this.nextId++, make, model, year);
+    let car = new Car(++this.nextId, make, model, year);
     this.list.push(car);
-    this.updateDOMWishList();
     }
   
     remove(carId) {
-    this.list = this.list.filter((car) => car.id != carId);
-    this.updateDOMWishList();
-    document.getElementById("car-make").textContent = "";
-    document.querySelector("#car-model").textContent = "";
-    document.querySelector("#car-year").textContent = "";
-    document.querySelector("#car-card").setAttribute("data-carId", "");
-
-    if (this.list.length == 0) {
-        document.querySelector(".removeBtn").disabled = true;
-    }
-    }
-
-    updateDOMWishList() {
-    let container = document.querySelector("#wishListContainer");
-    let ul = document.querySelector("ul");
-    ul.innerHTML = "";
-    this.list.forEach((car) => {
-        let li = document.createElement("li");
-        li.id = car.id;
-        li.textContent = car.model;
-        li.addEventListener("click", () => {
-        console.log("click");
-        document.querySelector(".removeBtn").disabled = false;
-        car.info();
-        });
-        ul.appendChild(li);
-    });
-    container.appendChild(ul);
+    this.list = this.list.filter(({id}) => id != carId);
     }
 }
